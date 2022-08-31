2d54870d3e5715814b30a24da79c82ffbcce062b
mervekavak <mervekvk6109@gmail.com>
2022-08-31 13:21:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+window.alert('I really love PİZZA');
+
+let students = 20;
+let names = "Merve";
+let test =true;
+
+console.log(typeof students);
+console.log(typeof names);
+if (test==true) {
+
+    students = students + 1;
+    names = names + "Muhammed";
+    document.getElementById("p1").innerHTML = "hellop1 " + names + " there are " + students + " students in the classroom";
+} else {
+    document.getElementById("p2").innerHTML = "hellop2 " + names + " there are " + students + " students in the classroom";
+
+}
+
+
+
+console.log("i like pizza");
+console.log(students);
+console.log(names);
+console.log(test);
